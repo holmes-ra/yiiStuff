616d2a41963d0cbb9f495cbb1632bdcec6925278
Ryan Holmes <ryan.xgamer99>
2012-11-23 22:20:26 +0000

    Key status column is now displayed via a seperate view to reduce clutter.

---
+++ protected/modules/user/views/profile/_keyGrid_statusCol.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!$data->isActive) {
+	echo "<a href='#' rel='tooltip' title='Inactive - please refresh key'><i class='icon-warning-sign'></i></a>"; }
+else { 
+/*
+	if ($data->info->accessMask != $data->activeAPIMask) {
+		echo "<a href='#' rel='tooltip' title='Access mismatch - please refresh key'><i class='icon-warning-sign'></i></a>"; }
+	else {*/
+		echo "<a href='#' rel='tooltip' title='OK'><i class='icon-ok'></i></a>"; }
+
+?>	
